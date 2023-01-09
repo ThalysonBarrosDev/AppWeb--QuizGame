f1739fe9c8de79c3f2c5ba2d03c78c6faf43ea01
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2023-01-09 12:48:53 -0300

    CHORE: Finalizando implementação do Quiz

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ScoreBoardComponent 
+    :winCount="this.winCount"
+    :loseCount="this.loseCount"
+  />
+
+  <div>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+  
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input :disabled="this.answerSubmitted" type="radio" name="options" :value="answer" v-model="this.chosenAnswer">
+        <label v-html="answer"></label><br/>
+      </template>
+  
+      <button v-if="!this.answerSubmitted" type="button" class="send" @click="submitAnswer()">Send</button>
+
+      <section v-if="this.answerSubmitted" class="result">
+        <h4 v-if="this.chosenAnswer == this.correctAnswer" v-html="'&#9989; Congratulations, the answer ' + this.correctAnswer + 'is correct.'"></h4>
+        <h4 v-else v-html="'&#10060; Im sorry, you picked the wrond answer. The correct is' + this.correctAnswer + '.'"></h4>
+
+        <button type="button" class="send" @click="this.getNewQuestion()">Next Question</button>
+      </section>
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScoreBoardComponent from './components/ScoreBoardComponent';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ScoreBoardComponent
+  },
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      winCount: 0,
+      loseCount: 0
+    }
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+
+      return answers;
+    }
+  },
+  methods: {
+    submitAnswer() {
+      if (!this.chosenAnswer) {
+        alert('Pick one of the options.');
+      } else {
+        this.answerSubmitted = true;
+
+        if (this.chosenAnswer == this.correctAnswer) {
+          this.winCount++;
+        } else {
+          this.loseCount++;
+        }
+      }
+    },
+    getNewQuestion() {
+      this.answerSubmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+
+      this.axios.get('https://opentdb.com/api.php?amount=1&category=18').then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswer = response.data.results[0].correct_answer;
+    });
+    }
+  },
+  created() {
+    this.getNewQuestion();
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family: Avenir, Arial, Helvetica, sans-serif;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
---
+++ src/components/ScoreBoardComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <section class="score">
+            Player <span>{{ this.winCount }}</span> x <span>{{ this.loseCount }}</span> Computer
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ScoreBoardComponent',
+    props: {
+        winCount: Number,
+        loseCount: Number
+    }
+}
+</script>
+
+<style lang="scss">
+section.score {
+    border-bottom: 1px solid black;
+    padding: 24px;
+    font-size: 18px;
+}
+
+section.score span {
+    padding: 8px;
+    font-weight: bold;
+    border: 1px solid black;
+}
+</style>
